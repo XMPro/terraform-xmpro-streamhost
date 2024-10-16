24b9b2f31502ef7673faca1d6a1e0d9b7c7a7bb8
Jonah Frany <jonah.frany@xmpro.com>
2024-10-16 09:55:02 +0800

    Cleanup variables

--- variables.tf
+++ variables.tf
@@ -12,20 +12,20 @@ variable "location" {
   default     = "australiaeast"
 }
 
-variable "docker_azure_openaichatdeploymentname" {
-  description = "Azure chat deployment name to be used by the stream host"
-  default     = "gpt4o"
-}
+# variable "docker_azure_openaichatdeploymentname" {
+#   description = "Azure chat deployment name to be used by the stream host"
+#   default     = "gpt4o"
+# }
 
-variable "docker_azure_openaiembeddingsdeploymentname" {
-  description = "Azure embeddings deployment name by the stream host"
-  default     = "text-embedding-ada-002"
-}
+# variable "docker_azure_openaiembeddingsdeploymentname" {
+#   description = "Azure embeddings deployment name by the stream host"
+#   default     = "text-embedding-ada-002"
+# }
 
-variable "docker_openaiversion" {
-  description = "Azure openai version by the stream host"
-  default     = "2024-02-01"
-}
+# variable "docker_openaiversion" {
+#   description = "Azure openai version by the stream host"
+#   default     = "2024-02-01"
+# }
 
 variable "docker_migration_script_path" {
   description = "value"
@@ -47,26 +47,26 @@ variable "storage_account_name" {
   type        = string
 }
 
-variable "storage_account_primary_access_key" {
-  description = "The storage account primary access key"
-  type        = string
-}
+# variable "storage_account_primary_access_key" {
+#   description = "The storage account primary access key"
+#   type        = string
+# }
 
-variable "streamhost_container_image" {
-  description = "The docker container image to be used by the stream host"
-  default     = "xmprocontrib.azurecr.io/sh-debian-ai-assistant:latest"
-}
+# variable "streamhost_container_image" {
+#   description = "The docker container image to be used by the stream host"
+#   default     = "xmprocontrib.azurecr.io/sh-debian-ai-assistant:latest"
+# }
 
-variable log_analytics_id {
-  description = "This variable holds the ID of the Log Analytics workspace"
-  default     = ""
-}
+# variable log_analytics_id {
+#   description = "This variable holds the ID of the Log Analytics workspace"
+#   default     = ""
+# }
 
-variable log_analytics_primary_shared_key {
-  description = "This variable holds the primary shared key for the Log Analytics workspace"
-  default     = ""
-}
+# variable log_analytics_primary_shared_key {
+#   description = "This variable holds the primary shared key for the Log Analytics workspace"
+#   default     = ""
+# }
 
-variable "container_streamhost_data" {
+# variable "container_streamhost_data" {
   
-}
+# }
