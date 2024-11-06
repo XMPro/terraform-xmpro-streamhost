0c09b09cbbd38f7648c697009a12b85f4f071fb6
Jonah Frany <jonahfrany129@gmail.com>
2024-11-06 05:49:54 +0000

    Branch: refs/heads/19265-pipeline-cleanup

--- azure_cg_sh.tf
+++ azure_cg_sh.tf
@@ -1,51 +1,47 @@
+
 resource "random_uuid" "sh_id" {
-  # for_each = { for key, value in var.container_streamhost_data : key => value }
 }
 
 resource "azurerm_container_group" "streamhost" {
-  # for_each = { for key, value in var.container_streamhost_data : key => value }
   name                = "cg-${var.streamhost_name}"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   ip_address_type     = "Public"
-  # dns_name_label      = "cg-${var.prefix}-${each.value.name}-${var.environment}-${var.location}"
-  os_type = "Linux"
+  os_type             = "Linux"
 
   container {
-    name   = "sh"
+    name = "sh"
     image  = var.streamhost_container_image
     cpu    = var.streamhost_cpu
     memory = var.streamhost_memory
 
-    environment_variables = {
-      "xm__xmpro__Gateway__Id" = random_uuid.sh_id.result
-      #"xm__xmpro__Gateway__Name"                  = format("SH-%02d-%s-ACI-%s", index(keys(var.container_streamhost_data), each.key) + 1, each.value.name, var.environment)
-      "xm__xmpro__Gateway__Name"                  = var.streamhost_name
-      "xm__xmpro__Gateway__ServerUrl"             = var.ds_server_url
-      "xm__xmpro__Gateway__CollectionId"          = var.streamhost_collection_id
-      "xm__xmpro__Gateway__Secret"                = var.streamhost_secret
-      "xm__ApplicationInsights__ConnectionString" = var.appinsights_connectionstring
-      # "AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"       = "${var.docker_azure_openaichatdeploymentname}"
-      # "AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME" = "${var.docker_azure_openaiembeddingsdeploymentname}"
-      # "OPENAI_API_VERSION"                      = "${var.docker_openaiversion}"
-      # "SCRIPT_PATH" = "${var.docker_migration_script_path}"
+    environment_variables = merge(
+      var.environment_variables,
+        {
+          "xm__xmpro__Gateway__Id"                    = random_uuid.sh_id.result
+          "xm__xmpro__Gateway__Name"                  = var.streamhost_name
+          "xm__xmpro__Gateway__ServerUrl"             = var.ds_server_url
+          "xm__xmpro__Gateway__CollectionId"          = var.streamhost_collection_id
+          "xm__xmpro__Gateway__Secret"                = var.streamhost_secret
+          "xm__ApplicationInsights__ConnectionString" = var.appinsights_connectionstring
+        }
+    )
+
+    dynamic "volume" {
+        for_each = var.use_existing_storage_account_share ? var.volumes : []
+        content {
+          name                  = volume.value.name
+          mount_path            = volume.value.mount_path
+          read_only             = try(volume.value.read_only, false)
+          share_name            = volume.value.share_name
+          storage_account_name  = volume.value.storage_account_name
+          storage_account_key   = volume.value.storage_account_key
+        }
     }
-
-    # volume {
-    #   name       = "rag-data"
-    #   mount_path = "/scripts"
-    #   read_only  = false
-    #   share_name = azurerm_storage_share.sh_data.name
-
-    #   storage_account_name = var.storage_account_name
-    #   storage_account_key  = var.storage_account_primary_access_key
-
-    # }
 
     ports {
       port     = 5000
       protocol = "TCP"
-
     }
   }
 
@@ -59,7 +55,7 @@ resource "azurerm_container_group" "streamhost" {
 
   tags = {
     product    = "XMPRO-${var.streamhost_name}"
-    createdby  = "XMPRO terraform streamhost module"
-    createdfor = "${var.prefix}-${var.environment}"
+    createdby  = "admin-${var.streamhost_name}"
+    createdfor = "${var.prefix}-${var.environment}-docker"
   }
 }
--- variables.tf
+++ variables.tf
@@ -12,26 +12,6 @@ variable "location" {
   default     = "australiaeast"
 }
 
-# variable "docker_azure_openaichatdeploymentname" {
-#   description = "Azure chat deployment name to be used by the stream host"
-#   default     = "gpt4o"
-# }
-
-# variable "docker_azure_openaiembeddingsdeploymentname" {
-#   description = "Azure embeddings deployment name by the stream host"
-#   default     = "text-embedding-ada-002"
-# }
-
-# variable "docker_openaiversion" {
-#   description = "Azure openai version by the stream host"
-#   default     = "2024-02-01"
-# }
-
-# variable "docker_migration_script_path" {
-#   description = "value"
-#   default     = "/scripts"
-# }
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -42,45 +22,19 @@ variable "resource_group_location" {
   type        = string
 }
 
-variable "storage_account_name" {
-  description = "The name of the storage account"
-  type        = string
-}
-
-variable "storage_account_primary_access_key" {
-  description = "The storage account primary access key"
-  type        = string
-}
-
-variable "streamhost_container_image" {
-  description = "The docker container image to be used by the stream host"
-  default     = "xmprocontrib.azurecr.io/sh-debian-ai-assistant:latest"
-}
-
-variable "log_analytics_id" {
-  description = "This variable holds the ID of the Log Analytics workspace"
-  default     = ""
-}
-
-variable "log_analytics_primary_shared_key" {
-  description = "This variable holds the primary shared key for the Log Analytics workspace"
-  default     = ""
-}
-
-variable "create_storage_share" {
-  description = "This variable is used to determine if the storage share should be created"
-  default     = false
-}
+#-----------------------
+# Streamhost information
+#-----------------------
 
 variable "streamhost_name" {
-  description = "The name of the streamhost"
-  default     = "SH-01-TFACI"
+  description = "The name of the stream host"
+  type        = string
 }
 
 variable "ds_server_url" {
-  description = "The URL of the DS site"
-  default     = "https://ds.xmpro.com"
-  type        = string
+  description = "The URL of the datastream host server (e.g., https://example.com)"
+  type        = string    
+  sensitive   = true
   validation {
     condition     = can(regex("^https?://", var.ds_server_url))
     error_message = "The ds_server_url must be a valid HTTP/HTTPS URL."
@@ -88,26 +42,77 @@ variable "ds_server_url" {
 }
 
 variable "streamhost_collection_id" {
-  description = "The collection ID for the streamhost"
-  default     = "default_ds_collection_id"
+  description = "The collection ID of the stream host"
+  type        = string
+  sensitive   = true
 }
 
 variable "streamhost_secret" {
-  description = "The secret for the streamhost"
-  default     = "default_ds_secret"
+  description = "The collection secret of the stream host"
+  type        = string
+  sensitive   = true
 }
 
 variable "appinsights_connectionstring" {
-  description = "The connection string for the Application Insights"
-  default     = "InstrumentationKey=00000000-0000-0000-0000-000000000000"
+  description = "The connection string of the application insights"
+  type        = string
+  sensitive   = true
 }
 
 variable "streamhost_cpu" {
-  description = "The container CPU(vcpu)for the streamhost"
-  default     = 1
+  description = "The CPU of the stream host"
+  type = number
+  validation {
+    condition     = var.streamhost_cpu >= 0.5 && var.streamhost_cpu <= 4
+    error_message = "CPU must be between 0.5 and 4 cores."
+  }
 }
 
 variable "streamhost_memory" {
-  description = "The container memory(GB) for the streamhost"
-  default     = 4
+  description = "The memory of the stream host"
+  type = number
+  validation {
+    condition     = var.streamhost_memory >= 1 && var.streamhost_memory <= 16
+    error_message = "Memory must be between 1 and 16 GB."
+  }
+}
+
+variable "streamhost_container_image" {
+  description = "The docker container image to be used by the stream host"
+  default     = "xmprocontrib.azurecr.io/sh-debian-ai-assistant:latest"
+}
+
+variable log_analytics_id {
+  description = "This variable holds the ID of the Log Analytics workspace"
+  type = string
+}
+
+variable log_analytics_primary_shared_key {
+  description = "This variable holds the primary shared key for the Log Analytics workspace"
+  type = string
+}
+
+variable "environment_variables" {
+  description = "The environment variables of the stream host"
+  type = map(string)
+  default = {}
+}
+
+variable "volumes" {
+  description = "List of volumes to be mounted"
+  type = list(object({
+    name                 = string
+    mount_path           = string
+    read_only            = optional(bool,false)
+    share_name           = string
+    storage_account_name = string
+    storage_account_key  = string
+  }))
+  default = []
+}
+
+variable "use_existing_storage_account_share" {
+  description = "set to true to use an existing storage account share, false to create a new one"
+  type = bool
+  default = true
 }
