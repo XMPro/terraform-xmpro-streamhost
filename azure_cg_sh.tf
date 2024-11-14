3393b8e739533bcc48370d7416b38325dacc60ce
Jonah Frany <jonahfrany129@gmail.com>
2024-11-14 06:59:11 +0000

    Branch: main

--- azure_cg_sh.tf
+++ azure_cg_sh.tf
@@ -9,34 +9,41 @@ resource "azurerm_container_group" "streamhost" {
   ip_address_type     = "Public"
   os_type             = "Linux"
 
+  image_registry_credential {
+    server   = var.acr_url
+    username = var.acr_username
+    password = var.acr_password
+  }
   container {
-    name = "sh"
+    name   = "sh"
     image  = var.streamhost_container_image
     cpu    = var.streamhost_cpu
     memory = var.streamhost_memory
 
+
+
     environment_variables = merge(
       var.environment_variables,
-        {
-          "xm__xmpro__Gateway__Id"                    = random_uuid.sh_id.result
-          "xm__xmpro__Gateway__Name"                  = var.streamhost_name
-          "xm__xmpro__Gateway__ServerUrl"             = var.ds_server_url
-          "xm__xmpro__Gateway__CollectionId"          = var.streamhost_collection_id
-          "xm__xmpro__Gateway__Secret"                = var.streamhost_secret
-          "xm__ApplicationInsights__ConnectionString" = var.appinsights_connectionstring
-        }
+      {
+        "xm__xmpro__Gateway__Id"                    = random_uuid.sh_id.result
+        "xm__xmpro__Gateway__Name"                  = var.streamhost_name
+        "xm__xmpro__Gateway__ServerUrl"             = var.ds_server_url
+        "xm__xmpro__Gateway__CollectionId"          = var.streamhost_collection_id
+        "xm__xmpro__Gateway__Secret"                = var.streamhost_secret
+        "xm__ApplicationInsights__ConnectionString" = var.appinsights_connectionstring
+      }
     )
 
     dynamic "volume" {
-        for_each = var.use_existing_storage_account_share ? var.volumes : []
-        content {
-          name                  = volume.value.name
-          mount_path            = volume.value.mount_path
-          read_only             = try(volume.value.read_only, false)
-          share_name            = volume.value.share_name
-          storage_account_name  = volume.value.storage_account_name
-          storage_account_key   = volume.value.storage_account_key
-        }
+      for_each = var.use_existing_storage_account_share ? var.volumes : []
+      content {
+        name                 = volume.value.name
+        mount_path           = volume.value.mount_path
+        read_only            = try(volume.value.read_only, false)
+        share_name           = volume.value.share_name
+        storage_account_name = volume.value.storage_account_name
+        storage_account_key  = volume.value.storage_account_key
+      }
     }
 
     ports {
--- variables.tf
+++ variables.tf
@@ -23,6 +23,29 @@ variable "resource_group_location" {
 }
 
 #-----------------------
+# Azure Container Registry information
+#-----------------------
+
+variable "acr_url" {
+  description = "The URL of the Azure Container Registry"
+  type        = string
+  default     = "xmpro.azurecr.io"
+}
+
+variable "acr_username" {
+  description = "The username of the Azure Container Registry"
+  type        = string
+  default     = "xmpro"
+}
+
+variable "acr_password" {
+  description = "The password of the Azure Container Registry"
+  type        = string
+  sensitive   = true
+  default     = "none"
+}
+
+#-----------------------
 # Streamhost information
 #-----------------------
 
@@ -34,7 +57,7 @@ variable "streamhost_name" {
 
 variable "ds_server_url" {
   description = "The URL of the datastream host server (e.g., https://example.com)"
-  type        = string    
+  type        = string
   sensitive   = true
   validation {
     condition     = can(regex("^https?://", var.ds_server_url))
@@ -58,13 +81,13 @@ variable "appinsights_connectionstring" {
   description = "The connection string of the application insights"
   type        = string
   sensitive   = true
-  default = "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
+  default     = "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
 }
 
 variable "streamhost_cpu" {
   description = "The CPU of the stream host"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
   validation {
     condition     = var.streamhost_cpu >= 0.5 && var.streamhost_cpu <= 4
     error_message = "CPU must be between 0.5 and 4 cores."
@@ -73,8 +96,8 @@ variable "streamhost_cpu" {
 
 variable "streamhost_memory" {
   description = "The memory of the stream host"
-  type = number
-  default = 4
+  type        = number
+  default     = 4
   validation {
     condition     = var.streamhost_memory >= 1 && var.streamhost_memory <= 16
     error_message = "Memory must be between 1 and 16 GB."
@@ -86,22 +109,22 @@ variable "streamhost_container_image" {
   default     = "xmprocontrib.azurecr.io/sh-debian-ai-assistant:latest"
 }
 
-variable log_analytics_id {
+variable "log_analytics_id" {
   description = "This variable holds the ID of the Log Analytics workspace"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
-variable log_analytics_primary_shared_key {
+variable "log_analytics_primary_shared_key" {
   description = "This variable holds the primary shared key for the Log Analytics workspace"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "environment_variables" {
   description = "The environment variables of the stream host"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "volumes" {
@@ -109,7 +132,7 @@ variable "volumes" {
   type = list(object({
     name                 = string
     mount_path           = string
-    read_only            = optional(bool,false)
+    read_only            = optional(bool, false)
     share_name           = string
     storage_account_name = string
     storage_account_key  = string
@@ -119,6 +142,6 @@ variable "volumes" {
 
 variable "use_existing_storage_account_share" {
   description = "set to true to use an existing storage account share, false to create a new one"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
